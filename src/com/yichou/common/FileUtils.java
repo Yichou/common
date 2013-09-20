e04a5c13381bfd205c1537bda6bc48ce38f6b0bb
Yichou <yichouangle@gmail.com>
2013-09-20 11:02:25 +0800

    fix something

--- src/com/yichou/common/FileUtils.java
+++ src/com/yichou/common/FileUtils.java
@@ -19,22 +19,23 @@ import android.util.Log;
 /**
  * 文件操作工具类
  * 
- * @author Yichou
+ * @author Yichou 
  * 
+ * @date 2013-08-31
  */
 public final class FileUtils {
-static final String LOG_TAG = "FileUtils";
-	
+	static final String LOG_TAG = "FileUtils";
+
 	/**
 	 * 操作成功返回值
 	 */
 	public static final int SUCCESS = 0;
-	
+
 	/**
 	 * 操作失败返回值
 	 */
 	public static final int FAILED = -1;
-	
+
 	private static final int BUF_SIZE = 32 * 1024; // 32KB
 
 	/**
@@ -45,23 +46,23 @@ static final String LOG_TAG = "FileUtils";
 		FState_File("I am file!"), // 文件
 		FState_None("I am a ghost!"), // 不存在
 		FState_Other("I am not human!"); // 其他类型
-		
+
 		private String tag;
-		
+
 		private FileState(String tag) {
 			this.tag = tag;
 		}
-		
+
 		public String getTag() {
 			return tag;
 		}
-		
+
 		@Override
 		public String toString() {
 			return tag;
 		}
 	}
-	
+
 	private FileUtils() {
 	}
 
@@ -87,7 +88,7 @@ static final String LOG_TAG = "FileUtils";
 
 		return FileState.FState_Other;
 	}
-	
+
 	/**
 	 * 创建文件夹
 	 * 
@@ -95,11 +96,12 @@ static final String LOG_TAG = "FileUtils";
 	 * @return
 	 */
 	public static int createDir(String path) {
-//		int l = path.length();
-//		if(path.charAt(l-1) == File.separatorChar){ //如果末尾是 / 会导致创建目录失败，测试发现不会
-//			path = path.substring(0, l-1);
-//		}
-		
+		// int l = path.length();
+		// if(path.charAt(l-1) == File.separatorChar){ //如果末尾是 /
+		// 会导致创建目录失败，测试发现不会
+		// path = path.substring(0, l-1);
+		// }
+
 		return createDir(new File(path));
 	}
 
@@ -119,72 +121,43 @@ static final String LOG_TAG = "FileUtils";
 	public static int removeDir(String path) {
 		return removeDir(new File(path));
 	}
-	
+
 	/**
 	 * 删除一个文件夹
 	 * 
-	 * <p>by:yichou 2013-5-7 15:24:41<p>
+	 * <p>
+	 * by:yichou 2013-5-7 15:24:41
+	 * <p>
 	 * 
 	 * @param dir
 	 * @return
 	 */
 	public static int removeDir(File dir) {
-		if(!dir.exists())
+		if (!dir.exists())
 			return SUCCESS;
-		
-		if(dir.isDirectory()){
+
+		if (dir.isDirectory()) {
 			File[] files = dir.listFiles();
 			if (files != null) {
 				for (File f : files) {
-					if(f.isDirectory())
+					if (f.isDirectory())
 						removeDir(f);
 					else
 						f.delete();
 				}
 			}
 		}
-		
-		return dir.delete()? SUCCESS : FAILED;
+
+		return dir.delete() ? SUCCESS : FAILED;
 	}
 
-	/**
-	 * 获取文件的目录路径. 需要注意的是输入的路径是完整的文件名路径. 否则会导致解析出错<br>
-	 * 路径的统一方式为:<br>
-	 * CmmnFiles.SDCardFolder + "..."<br>
-	 * 或者:<br>
-	 * CmmnFiles.InnerStortageFolder + "..."
-	 * 
-	 * @param path
-	 * 
-	 * @return 父目录 或者 null 不存在父目录
-	 */
-	public static String getFolderFromPath(String path) {
-		return getFolderFromPath(new File(path));
-	}
-
-	public static String getFolderFromPath(File file) {
-		if (!file.isAbsolute())
-			return null;
-
-		String path = file.getAbsolutePath();
-		String[] slipArray = path.split(File.separator);
-		// 错误的格式
-		if (slipArray.length <= 0)
-			return null;
-		// /XXX/YYY/ZZZ.CCC
-		int pathLen = path.length();
-		int fileNameLen = slipArray[slipArray.length - 1].length();
-
-		return path.substring(0, pathLen - fileNameLen);
-	}
-	
 	/**
 	 * @see {@link #checkParentPath(File)}
 	 */
 	public static void checkParentPath(String path) {
 		checkParentPath(new File(path));
 	}
-	
+
 	/**
 	 * 在打开一个文件写数据之前，先检测该文件路径的父目录是否已创建，保证能创建文件
 	 * 
@@ -201,8 +174,10 @@ static final String LOG_TAG = "FileUtils";
 	 * 
 	 * @param path
 	 *            目标文件路径
-	 * @param is 输入流
-	 * @param isAppend 是否追加
+	 * @param is
+	 *            输入流
+	 * @param isAppend
+	 *            是否追加
 	 * 
 	 * @return 成功 {@link #SUCCESS}； 失败 {@link #FAILED}
 	 */
@@ -234,25 +209,31 @@ static final String LOG_TAG = "FileUtils";
 
 		return FAILED;
 	}
-	
+
 	/**
 	 * 写字节数组到文件
 	 * 
-	 * @param file 目标文件
-	 * @param data 字节数组
-	 * @param offset 偏移 {@code >=0&&<=data.length}
-	 * @param length 长度 ==0 表示 data.length
-	 * @param isAppend 是否追加
+	 * @param file
+	 *            目标文件
+	 * @param data
+	 *            字节数组
+	 * @param offset
+	 *            偏移 {@code >=0&&<=data.length}
+	 * @param length
+	 *            长度 ==0 表示 data.length
+	 * @param isAppend
+	 *            是否追加
 	 * 
 	 * @return 成功 {@link #SUCCESS}； 失败 {@link #FAILED}
 	 */
-	public static int bytesToFile(File file, byte[] data, int offset, int length, boolean isAppend) {
+	public static int bytesToFile(File file, byte[] data, int offset,
+			int length, boolean isAppend) {
 		checkParentPath(file);
-		
-		if(data == null)
+
+		if (data == null)
 			return FAILED;
-		
-		if(length <= 0)
+
+		if (length <= 0)
 			length = data.length;
 
 		FileOutputStream fos = null;
@@ -260,7 +241,7 @@ static final String LOG_TAG = "FileUtils";
 			fos = new FileOutputStream(file, isAppend);
 			fos.write(data, offset, length);
 			fos.flush();
-			
+
 			return SUCCESS;
 		} catch (Exception e) {
 		} finally {
@@ -272,56 +253,64 @@ static final String LOG_TAG = "FileUtils";
 
 		return FAILED;
 	}
-	
-	public static int bytesToFile(File file, byte[] data){
+
+	public static int bytesToFile(File file, byte[] data) {
 		return bytesToFile(file, data, 0, data.length, false);
 	}
-	
+
 	/**
 	 * @see {@link #bytesToFile(File file, byte[] data, int offset, int length, boolean isAppend)}
 	 */
-	public static int bytesToFile(String path, byte[] data, int offset, int length, boolean isAppend) {
+	public static int bytesToFile(String path, byte[] data, int offset,
+			int length, boolean isAppend) {
 		return bytesToFile(new File(path), data, offset, length, isAppend);
 	}
-	
+
 	/**
 	 * 读取文件内容到二进制缓冲区
 	 * 
-	 * @param path 文件路径
-	 * @param offset 起始位置
-	 * @param length 读取长度 ，0为全部
+	 * @param path
+	 *            文件路径
+	 * @param offset
+	 *            起始位置
+	 * @param length
+	 *            读取长度 ，0为全部
 	 * 
 	 * @return 失败 或 length <=0 返回null，成功返回 字节数组
 	 */
 	public static byte[] fileToBytes(String path, int offset, int length) {
 		return fileToBytes(new File(path), offset, length);
 	}
-	
-	public static byte[] fileToBytes(File file){
+
+	public static byte[] fileToBytes(File file) {
 		return fileToBytes(file, 0, 0);
 	}
+
 	/**
 	 * 读取文件内容到二进制缓冲区
 	 * 
-	 * @param path 文件路径
-	 * @param offset 起始位置
-	 * @param length 读取长度，==0 为全部
+	 * @param path
+	 *            文件路径
+	 * @param offset
+	 *            起始位置
+	 * @param length
+	 *            读取长度，==0 为全部
 	 * 
-	 * @return 失败 或 length < 0  返回null，成功返回 字节数组
+	 * @return 失败 或 length < 0 返回null，成功返回 字节数组
 	 */
 	public static byte[] fileToBytes(File file, int offset, int length) {
-		if( length < 0 )
+		if (length < 0)
 			return null;
-		
+
 		InputStream is = null;
-		
+
 		try {
 			is = new FileInputStream(file);
-			if(length == 0)
+			if (length == 0)
 				length = is.available();
 			byte[] outBuf = new byte[length];
 			is.read(outBuf, offset, length);
-			
+
 			return outBuf;
 		} catch (Exception e) {
 		} finally {
@@ -330,7 +319,7 @@ static final String LOG_TAG = "FileUtils";
 			} catch (Exception e) {
 			}
 		}
-		
+
 		return null;
 	}
 
@@ -376,9 +365,11 @@ static final String LOG_TAG = "FileUtils";
 	/**
 	 * assets 目录下的文件保存到本地文件
 	 * 
-	 * @param context 
-	 * @param assetName assets下名字，非根目录需包含路径 a/b.xxx
-	 * @param file 目标文件
+	 * @param context
+	 * @param assetName
+	 *            assets下名字，非根目录需包含路径 a/b.xxx
+	 * @param file
+	 *            目标文件
 	 * 
 	 * @return 成功 {@link #SUCCESS}； 失败 {@link #FAILED}
 	 */
@@ -398,7 +389,7 @@ static final String LOG_TAG = "FileUtils";
 
 		return FAILED;
 	}
-	
+
 	/**
 	 * 从 Assets 文件读取文件全部，并转为字符串
 	 * 
@@ -408,28 +399,30 @@ static final String LOG_TAG = "FileUtils";
 	 * @return 读取到的字符串
 	 * 
 	 * @author Yichou
-	 * <p>date 2013-4-2 11:30:05
+	 *         <p>
+	 *         date 2013-4-2 11:30:05
 	 */
-    public static String readStringFromAsset(AssetManager manager, String name){
+	public static String readStringFromAsset(AssetManager manager, String name) {
 		InputStream is = null;
-		
+
 		try {
 			is = manager.open(name);
 			byte[] buf = new byte[is.available()];
 			is.read(buf);
-			
+
 			return new String(buf);
 		} catch (Exception e) {
-//			FsSdkLog.e(LOG_TAG, "readStringFromAsset(" + name + ") error!\n" + e.getMessage());
+			// FsSdkLog.e(LOG_TAG, "readStringFromAsset(" + name + ") error!\n"
+			// + e.getMessage());
 		} finally {
 			try {
 				is.close();
 			} catch (Exception e) {
 			}
 		}
-		
+
 		return "";
-    }
+	}
 
 	/**
 	 * 检查 assets 下是否存在某文件
@@ -469,11 +462,13 @@ static final String LOG_TAG = "FileUtils";
 		if (isSDMounted() == false) {
 			return 0;
 		} else {
-			StatFs statfs = new StatFs(Environment.getExternalStorageDirectory() + File.separator);
-			return (long) statfs.getAvailableBlocks() * (long) statfs.getBlockSize();
+			StatFs statfs = new StatFs(
+					Environment.getExternalStorageDirectory() + File.separator);
+			return (long) statfs.getAvailableBlocks()
+					* (long) statfs.getBlockSize();
 		}
 	}
-	
+
 	public static String coverSize(long size) {
 		String s = "";
 		if (size < 1024)
@@ -487,18 +482,18 @@ static final String LOG_TAG = "FileUtils";
 		}
 		return s;
 	}
-	
+
 	public static long getROMLeft() {
 		File data = Environment.getDataDirectory();
-		
+
 		StatFs sf = new StatFs(data.getAbsolutePath());
 		long blockSize = sf.getBlockSize();
 		long blockCount = sf.getBlockCount();
 		long availCount = sf.getAvailableBlocks();
-		
-		Log.i("", "ROM Total:" + coverSize(blockSize * blockCount) 
-				+ ", Left:" + coverSize(availCount * blockSize));
-		
+
+		Log.i("", "ROM Total:" + coverSize(blockSize * blockCount) + ", Left:"
+				+ coverSize(availCount * blockSize));
+
 		return availCount * blockSize;
 	}
 
@@ -511,9 +506,10 @@ static final String LOG_TAG = "FileUtils";
 	 * @return
 	 */
 	public static String getDirPathInPrivate(Context context, String name) {
-		return context.getDir(name, Context.MODE_PRIVATE).getAbsolutePath() + File.separator;
+		return context.getDir(name, Context.MODE_PRIVATE).getAbsolutePath()
+				+ File.separator;
 	}
-	
+
 	/**
 	 * 或者本应用 so 存放路径
 	 * 
@@ -523,11 +519,11 @@ static final String LOG_TAG = "FileUtils";
 	public static String getSoPath(Context context) {
 		return context.getApplicationInfo().dataDir + "/lib/";
 	}
-	
+
 	public static FileLock tryFileLock(String path) {
 		return tryFileLock(new File(path));
 	}
-	
+
 	/**
 	 * 占用某个文件锁
 	 * 
@@ -536,34 +532,35 @@ static final String LOG_TAG = "FileUtils";
 	 */
 	public static FileLock tryFileLock(File file) {
 		try {
-			checkParentPath(file); //父目录不存在会导致创建文件锁失败
+			checkParentPath(file); // 父目录不存在会导致创建文件锁失败
 
 			FileOutputStream fos = new FileOutputStream(file);
 			FileLock fl = fos.getChannel().tryLock();
-			if(fl.isValid()) {
+			if (fl.isValid()) {
 				Log.i(LOG_TAG, "tryFileLock " + file + " SUC!");
 				return fl;
 			}
 		} catch (Exception e) {
 			Log.e(LOG_TAG, "tryFileLock " + file + " FAIL! " + e.getMessage());
 		}
-		
+
 		return null;
 	}
-	
+
 	public static void freeFileLock(FileLock fl, File file) {
-		if(file != null)
+		if (file != null)
 			file.delete();
 
-		if(fl == null || !fl.isValid()) return;
-		
+		if (fl == null || !fl.isValid())
+			return;
+
 		try {
 			fl.release();
 			Log.i(LOG_TAG, "freeFileLock " + file + " SUC!");
 		} catch (IOException e) {
 		}
 	}
-	
+
 	/**
 	 * 截取路径名
 	 * 
@@ -574,7 +571,7 @@ static final String LOG_TAG = "FileUtils";
 		int end = absolutePath.length();
 		return absolutePath.substring(start, end);
 	}
-	
+
 	/**
 	 * 重命名
 	 * 
@@ -586,7 +583,7 @@ static final String LOG_TAG = "FileUtils";
 		File f = new File(oldName);
 		return f.renameTo(new File(newName));
 	}
-	
+
 	/**
 	 * 列出root目录下所有子目录
 	 * 
@@ -607,7 +604,7 @@ static final String LOG_TAG = "FileUtils";
 		}
 		return allDir;
 	}
-	
+
 	/**
 	 * 删除空目录
 	 * 
@@ -628,13 +625,14 @@ static final String LOG_TAG = "FileUtils";
 		}
 		return 3;
 	}
-	
+
 	/**
 	 * 获取SD卡的根目录，末尾带\
 	 * 
 	 * @return
 	 */
 	public static String getSDRoot() {
-		return Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator;
+		return Environment.getExternalStorageDirectory().getAbsolutePath()
+				+ File.separator;
 	}
 }
